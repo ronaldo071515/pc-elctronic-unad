5ea9d6ef56703c36cf2d03fd5acb8f8b365787a4
zacksanz <64340254+zacksanz@users.noreply.github.com>
2021-11-12 23:59:28 -0500

    test: add debugging features

---
+++ includes/utilities/debugging.php
@@ -0,0 +1,34 @@
+<?php
+
+//Apagar la salida del debugger
+$DEBUG_MODE = TRUE;
+
+/* Clase para debugging */
+class Debugger{
+    private $debug_messages; // = array();
+
+    public function __construct(){
+        $this->debug_messages = array();
+    }
+    public function out($message){
+        $this->debug_messages[] = $message;
+        if($GLOBALS['DEBUG_MODE']){
+            echo "-- Debugger: \"".$message."\"\n";
+        }
+    }
+    public function get_trace(){
+        return $this->debug_messages;
+    }
+}
+
+$debugger = new Debugger();
+
+if($DEBUG_MODE){
+    error_reporting(E_ALL);
+    ini_set('display_errors', '1');
+}else {
+    error_reporting(~E_ALL);
+    ini_set('display_errors', '0');
+}
+
+?>
